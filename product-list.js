172cd5d0d23a9c136eb90d6048734d9b07af613e
ycodeneed <119536867+ycodeneed@users.noreply.github.com>
2025-01-20 15:00:53 +0530

    Update product-list.js

--- product-list.js
+++ product-list.js
@@ -30,8 +30,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
     STORE.init();
     STORE.productList = document.getElementById('product-list');
 
-    setTimeout(() => {STORE.loader.remove();encodeLib.showSkeletonLoader();}, 500);
-    setTimeout(() => {encodeLib.renderProducts(Object.values(products));}, 1500);
+    setTimeout(() => {STORE.loader.remove();STORE.showSkeletonLoader();}, 700);
+    setTimeout(() => {STORE.renderProducts(Object.values(products));}, 1700);
 });
 
 var STORE = {
@@ -39,96 +39,23 @@ var STORE = {
         let loaderDiv = encodeLib.loader({ id: "loader", position: "absolute", backgroundColor: "#f9f9f9", zIndex: "6000", top: "0", left: "0" });
         encodeLib.insert(encodeLib.BODY, loaderDiv, { addOn: "prepend" });
 
+        encodeLib.outerClickFunctions.push(STORE.sitebarOuterClickFunc);
+
         var categoryFilter = document.getElementById('categoryFilter');
         var sortOptions = document.getElementById('sortOptions');
-
-        categoryFilter.addEventListener('change', encodeLib.filterAndSortProducts);
-        sortOptions.addEventListener('change', encodeLib.filterAndSortProducts);
+        categoryFilter.addEventListener('change', STORE.filterAndSortProducts);
+        sortOptions.addEventListener('change', STORE.filterAndSortProducts);
         
-        document.getElementById('nonePrice').addEventListener('change', encodeLib.filterAndSortProducts);
-        document.getElementById('priceUnder250').addEventListener('change', encodeLib.filterAndSortProducts);
-        document.getElementById('price250to500').addEventListener('change', encodeLib.filterAndSortProducts);
-        document.getElementById('priceAbove500').addEventListener('change', encodeLib.filterAndSortProducts);
-        
+        document.getElementById('nonePrice').addEventListener('change', STORE.filterAndSortProducts);
+        document.getElementById('priceUnder250').addEventListener('change', STORE.filterAndSortProducts);
+        document.getElementById('price250to500').addEventListener('change', STORE.filterAndSortProducts);
+        document.getElementById('priceAbove500').addEventListener('change', STORE.filterAndSortProducts);
+        $(".side-bar-button").click(function() {encodeLib.toggleEvent(this, STORE.openFilterBar, STORE.closeFilterBar);});
 
         STORE.loader = $("#loader");
         STORE.HTML = $("#outerBody");
         STORE.BODY = $("#outerBody").children(".content").first();
 
-    },
-    appElement: function (appInfoObj, listId) {
-
-        if (!appInfoObj || !listId) {
-            return;
-        }
-
-        if(appInfoObj.hideApp) {
-            appInfoObj.helpDocLink[platform] = "";
-        }
-
-        let appElement = `<div class="product" id="${appInfoObj.id}">
-                                <div class="product-inner" title="${appInfoObj.name}">
-                                    <div id="skeletonLoader" class="skeletonLoader">
-                                        <div class="skeleton skeleton-image"></div>
-                                        <div class="skeleton skeleton-title"></div>
-                                        <div class="skeleton skeleton-price"></div>
-                                        <div class="skeleton skeleton-button"></div>
-                                    </div>
-                                    <div>
-                                        <div class="product-image">
-                                            <span>
-                                                <a class="product-image-link" target="_blank" href="#">
-                                                    <div class="product-image-inner">
-                                                        <img src="${appInfoObj.image ? appInfoObj.image : '/shopping.png'}" alt="${appInfoObj.name}" class="lazy-image" onload="$('#'+${appInfoObj.id}+' .skeletonLoader').remove();">
-                                                    </div>
-                                                </a>
-                                            </span>
-                                        </div>
-                                        <div class="product-name">${appInfoObj.name}</div>
-                                        <div class="product-price">
-                                            <span class="product-price-inner">
-                                                <span class="product-price-symbol">₹</span>
-                                                <span class="product-price-whole">${appInfoObj.price}</span>
-                                            </span>
-                                        </div>
-                                        <div class="product-addCart">
-                                            <button onclick="encodeLib.rippleAction(this, event);" class="product-addCart-button product-Cart-button">
-                                                <svg class="cart-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve"> <defs> </defs> <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"> <circle cx="33.718" cy="73.438" r="6.848" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform="  matrix(1 0 0 1 0 0) "></circle> <circle cx="69.928" cy="73.438" r="6.848" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform="  matrix(1 0 0 1 0 0) "></circle> <path d="M 16.209 25.922 c -1.348 0 -2.563 -0.935 -2.864 -2.306 l -0.542 -2.467 c -0.708 -3.225 -3.617 -5.565 -6.919 -5.565 h -2.95 C 1.314 15.583 0 14.269 0 12.649 s 1.314 -2.935 2.935 -2.935 h 2.95 c 6.037 0 11.357 4.28 12.651 10.176 l 0.542 2.467 c 0.348 1.583 -0.654 3.148 -2.237 3.496 C 16.629 25.9 16.417 25.922 16.209 25.922 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 60.166 73.837 c -0.005 -0.133 -0.02 -0.264 -0.02 -0.398 c 0 -2.025 0.619 -3.908 1.677 -5.471 H 41.825 c 1.058 1.563 1.677 3.446 1.677 5.471 c 0 0.134 -0.015 0.265 -0.02 0.398 H 60.166 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 23.937 73.438 c 0 -2.036 0.626 -3.927 1.695 -5.495 c -1.377 -0.121 -2.464 -1.267 -2.464 -2.674 c 0 -1.487 1.21 -2.698 2.698 -2.698 c 1.621 0 2.935 -1.314 2.935 -2.935 c 0 -1.621 -1.314 -2.935 -2.935 -2.935 c -4.725 0 -8.568 3.843 -8.568 8.567 c 0 4.063 2.846 7.468 6.648 8.343 C 23.945 73.554 23.937 73.497 23.937 73.438 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 83.903 70.902 L 83.903 70.902 c 0 -1.621 -1.314 -2.935 -2.935 -2.935 h -2.808 c 1.058 1.563 1.677 3.446 1.677 5.471 c 0 0.134 -0.015 0.265 -0.02 0.398 h 1.151 C 82.589 73.837 83.903 72.523 83.903 70.902 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 89.251 21.027 c -0.557 -0.621 -1.352 -0.976 -2.186 -0.976 H 16.211 c -0.889 0 -1.73 0.403 -2.287 1.095 c -0.557 0.693 -0.77 1.601 -0.579 2.469 l 8.043 36.651 c 0.295 1.346 1.488 2.305 2.866 2.305 h 53.688 c 4.965 0 8.139 -3.722 8.681 -8.658 l 3.359 -30.607 C 90.073 22.478 89.807 21.649 89.251 21.027 z M 61.201 44 H 56 v 5.201 c 0 1.657 -1.343 3 -3 3 s -3 -1.343 -3 -3 V 44 h -5.201 c -1.657 0 -3 -1.343 -3 -3 s 1.343 -3 3 -3 H 50 v -5.201 c 0 -1.657 1.343 -3 3 -3 s 3 1.343 3 3 V 38 h 5.201 c 1.657 0 3 1.343 3 3 S 62.858 44 61.201 44 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> </g> </svg>
-                                                <span class="cart-item">Add to cart</span>
-                                                <span class="ripple"></span>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`;
-
-        encodeLib.insert(listId, appElement, { addOn: "append" });
-        // $(".skeletonLoader").remove();
-
-    }
-};
-
-
-var encodeLib = {
-    init: function() {
-
-        document.body.parentNode.style.width = "100%";
-        document.body.parentNode.style.height = "100%";
-        document.body.parentNode.style.margin = "0";
-        document.body.parentNode.style.overflow = "hidden";
-        document.body.parentNode.style.position = "relative";
-
-        document.body.style.width = "100%";
-        document.body.style.height = "100%";
-        document.body.style.margin = "0";
-        document.body.style.overflow = "hidden";
-        document.body.style.position = "relative";
-
-        encodeLib.BODY = $("body");
-        encodeLib.HTML = $("html");
-
-        document.onclick = encodeLib.outerClickFunction;
-        
     },
     renderProducts: function(filteredProducts) {
         STORE.productList.innerHTML = '';
@@ -136,7 +63,7 @@ var encodeLib = {
             let thisAppObj = JSON.parse(JSON.stringify(appObj));
             STORE.appElement(thisAppObj, $("#product-list"));
         });
-        encodeLib.lazyLoadImages();
+        STORE.lazyLoadImages();
     },
     filterAndSortProducts: function() {
 
@@ -167,8 +94,24 @@ var encodeLib = {
           filteredProducts.sort((a, b) => b.price - a.price);
         }
     
-        encodeLib.renderProducts(filteredProducts);
+        STORE.renderProducts(filteredProducts);
         
+    },
+    sitebarOuterClickFunc: function(e) {
+        let thisElement = $(".filter-bar");
+        let thisElementExtra = $(".side-bar-button");
+        if(!thisElement.is(e.target) && thisElement.has(e.target).length === 0 && !thisElementExtra.is(e.target) && thisElementExtra.has(e.target).length === 0) {
+            $(".side-bar-button").attr("on", "false");
+            STORE.closeFilterBar();
+        }
+    },
+    openFilterBar: function(thisElement) {
+        $('.filter-bar').width("256px");
+        $('.outerBody').width("calc(100% - 256px)");
+    },
+    closeFilterBar: function(thisElement) {
+        $('.filter-bar').width("0");
+        $('.outerBody').width("calc(100% - 0px)");
     },
     showSkeletonLoader: function() {
         STORE.productList.innerHTML = '';
@@ -223,6 +166,80 @@ var encodeLib = {
             document.querySelector(".platformInfoAction").style.transform = "scale(0)";
         }, 1000);
     },
+    appElement: function (appInfoObj, listId) {
+
+        if (!appInfoObj || !listId) {
+            return;
+        }
+
+        if(appInfoObj.hideApp) {
+            appInfoObj.helpDocLink[platform] = "";
+        }
+
+        let appElement = `<div class="product" id="${appInfoObj.id}">
+                                <div class="product-inner" title="${appInfoObj.name}">
+                                    <div id="skeletonLoader" class="skeletonLoader">
+                                        <div class="skeleton skeleton-image"></div>
+                                        <div class="skeleton skeleton-title"></div>
+                                        <div class="skeleton skeleton-price"></div>
+                                        <div class="skeleton skeleton-button"></div>
+                                    </div>
+                                    <div>
+                                        <div class="product-image">
+                                            <span>
+                                                <a class="product-image-link" target="_blank" href="#">
+                                                    <div class="product-image-inner">
+                                                        <img src="${appInfoObj.image ? appInfoObj.image : '/shopping.png'}" alt="${appInfoObj.name}" class="lazy-image" onload="$('#'+${appInfoObj.id}+' .skeletonLoader').remove();">
+                                                    </div>
+                                                </a>
+                                            </span>
+                                        </div>
+                                        <div class="product-name">${appInfoObj.name}</div>
+                                        <div class="product-price">
+                                            <span class="product-price-inner">
+                                                <span class="product-price-symbol">₹</span>
+                                                <span class="product-price-whole">${appInfoObj.price}</span>
+                                            </span>
+                                        </div>
+                                        <div class="product-addCart">
+                                            <button onclick="STORE.rippleAction(this, event);" class="product-addCart-button product-Cart-button">
+                                                <svg class="cart-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 256 256" xml:space="preserve"> <defs> </defs> <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)"> <circle cx="33.718" cy="73.438" r="6.848" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform="  matrix(1 0 0 1 0 0) "></circle> <circle cx="69.928" cy="73.438" r="6.848" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform="  matrix(1 0 0 1 0 0) "></circle> <path d="M 16.209 25.922 c -1.348 0 -2.563 -0.935 -2.864 -2.306 l -0.542 -2.467 c -0.708 -3.225 -3.617 -5.565 -6.919 -5.565 h -2.95 C 1.314 15.583 0 14.269 0 12.649 s 1.314 -2.935 2.935 -2.935 h 2.95 c 6.037 0 11.357 4.28 12.651 10.176 l 0.542 2.467 c 0.348 1.583 -0.654 3.148 -2.237 3.496 C 16.629 25.9 16.417 25.922 16.209 25.922 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 60.166 73.837 c -0.005 -0.133 -0.02 -0.264 -0.02 -0.398 c 0 -2.025 0.619 -3.908 1.677 -5.471 H 41.825 c 1.058 1.563 1.677 3.446 1.677 5.471 c 0 0.134 -0.015 0.265 -0.02 0.398 H 60.166 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 23.937 73.438 c 0 -2.036 0.626 -3.927 1.695 -5.495 c -1.377 -0.121 -2.464 -1.267 -2.464 -2.674 c 0 -1.487 1.21 -2.698 2.698 -2.698 c 1.621 0 2.935 -1.314 2.935 -2.935 c 0 -1.621 -1.314 -2.935 -2.935 -2.935 c -4.725 0 -8.568 3.843 -8.568 8.567 c 0 4.063 2.846 7.468 6.648 8.343 C 23.945 73.554 23.937 73.497 23.937 73.438 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 83.903 70.902 L 83.903 70.902 c 0 -1.621 -1.314 -2.935 -2.935 -2.935 h -2.808 c 1.058 1.563 1.677 3.446 1.677 5.471 c 0 0.134 -0.015 0.265 -0.02 0.398 h 1.151 C 82.589 73.837 83.903 72.523 83.903 70.902 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> <path d="M 89.251 21.027 c -0.557 -0.621 -1.352 -0.976 -2.186 -0.976 H 16.211 c -0.889 0 -1.73 0.403 -2.287 1.095 c -0.557 0.693 -0.77 1.601 -0.579 2.469 l 8.043 36.651 c 0.295 1.346 1.488 2.305 2.866 2.305 h 53.688 c 4.965 0 8.139 -3.722 8.681 -8.658 l 3.359 -30.607 C 90.073 22.478 89.807 21.649 89.251 21.027 z M 61.201 44 H 56 v 5.201 c 0 1.657 -1.343 3 -3 3 s -3 -1.343 -3 -3 V 44 h -5.201 c -1.657 0 -3 -1.343 -3 -3 s 1.343 -3 3 -3 H 50 v -5.201 c 0 -1.657 1.343 -3 3 -3 s 3 1.343 3 3 V 38 h 5.201 c 1.657 0 3 1.343 3 3 S 62.858 44 61.201 44 z" style="stroke: none;stroke-width: 1;stroke-dasharray: none;stroke-linecap: butt;stroke-linejoin: miter;stroke-miterlimit: 10;fill: rgb(255 255 255);fill-rule: nonzero;opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"></path> </g> </svg>
+                                                <span class="cart-item">Add to cart</span>
+                                                <span class="ripple"></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+
+        encodeLib.insert(listId, appElement, { addOn: "append" });
+        // $(".skeletonLoader").remove();
+
+    }
+};
+
+
+var encodeLib = {
+    init: function() {
+
+        document.body.parentNode.style.width = "100%";
+        document.body.parentNode.style.height = "100%";
+        document.body.parentNode.style.margin = "0";
+        document.body.parentNode.style.overflow = "hidden";
+        document.body.parentNode.style.position = "relative";
+
+        document.body.style.width = "100%";
+        document.body.style.height = "100%";
+        document.body.style.margin = "0";
+        document.body.style.overflow = "hidden";
+        document.body.style.position = "relative";
+
+        encodeLib.BODY = $("body");
+        encodeLib.HTML = $("html");
+
+        document.onclick = encodeLib.outerClickFunction;
+        
+    },
     toggleEvent: function(thisElement, trueFunction, falseFunction, onName="on") {
         if(!thisElement) {
             return;
@@ -238,14 +255,6 @@ var encodeLib = {
             thisElement.attr(onName, "true");
             trueFunction(thisElement);
         }
-    },
-    openFilterBar: function(thisElement) {
-        $('.filter-bar').width("256px");
-        $('.outerBody').width("calc(100% - 256px)");
-    },
-    closeFilterBar: function(thisElement) {
-        $('.filter-bar').width("0");
-        $('.outerBody').width("calc(100% - 0px)");
     },
     insert: function(parentElement, addElements="", insertObject = {}) {
         if(!parentElement) {
